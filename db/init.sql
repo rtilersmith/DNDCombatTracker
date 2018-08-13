16f8e9e4096f0beaccc6650578761769132b9726
Tiler Smith <TilerSmith@MacBook-Air.local>
2018-08-13 11:53:57 -0600

    set init tables, added ducks files

---
+++ db/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE CombatantInfo (
+	ID SERIAL PRIMARY KEY,
+	Name VARCHAR(30),
+	AC INTEGER,
+	HP INTEGER,
+	Str INTEGER,
+	Dex INTEGER,
+	Con INTEGER,
+	Wis INTEGER,
+	Int INTEGER,
+	Cha INTEGER,
+	Initiative INTEGER,
+	Skills VARCHAR(250)
+);
+
+CREATE TABLE Combatants (
+	Current_HP INTEGER,
+	Current_Init INTEGER,
+	Character_ID INTEGER references CombatantInfo
+);
---
+++ src/Ducks/store.js
@@ -0,0 +1,6 @@
+import { createStore, applyMiddleware } from 'redux'
+import reducer from './reducer'
+import promiseMiddleware from 'redux-promise-middleware'
+
+
+export default createStore(reducer, applyMiddleware(promiseMiddleware()))
