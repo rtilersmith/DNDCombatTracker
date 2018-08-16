b38badb2c438208fb4f12156b63caac95e127375
Tiler Smith <TilerSmith@MacBook-Air.local>
2018-08-16 15:06:02 -0600

    player info rendering on combat page

--- src/Components/CombatPage/CombatPage.js
+++ src/Components/CombatPage/CombatPage.js
@@ -1,15 +1,35 @@
 import React, { Component } from 'react'
-import Selected from '../Selected/Selected'
+import { connect } from 'react-redux'
+import { changeHealth } from '../../Ducks/player'
 
 class CombatPage extends Component{
 	render(props){
+		let {name, health, ac, init, strength, dex, con, wis, intel, cha, curHealth}=this.props
 		return (
 			<div>
-				CombatPage
-				<Selected />
+				{name}<br/>
+				{health}<br/>
+				{ac}<br/>
+				{init}<br/>
+				{strength}<br/>
+				{dex}<br/>
+				{con}<br/>
+				{wis}<br/>
+				{intel}<br/>
+				{cha}<br/>
+				{curHealth}<br/>
+
 			</div>
 		)
 	}
 }
 
-export default CombatPage
+let mapStateToProps=(state)=>{
+	let {name, health, ac, init, strength, dex, con, wis, intel, cha, curHealth}=state.player
+	return {
+		name, health, ac, init, strength, dex, con, wis, intel, cha, curHealth,
+		gmView: state.gm.curHealth
+	}
+}
+
+export default connect(mapStateToProps, {changeHealth})(CombatPage)
